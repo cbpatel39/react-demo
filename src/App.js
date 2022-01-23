d3764d24a29d8ae8c33bc28174e67c4cad9b3faf
cbpatel39 <cbpatel39@gmail.com>
2022-01-23 20:24:19 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import './App.css';
+import './assets/css/style.css';
+import './assets/css/responsive_style.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route}
 	from 'react-router-dom';
@@ -12,13 +17,13 @@ import Contact from './components/Pages/contact';
 function App() {
 return (
 	<Router>
-	<Navbar />
-	<Routes>
-		<Route exact path='/' exact element={<Home />} />
-		<Route path='/about' element={<About/>} />
-		<Route path='/contact' element={<Contact/>} />
-		<Route path='/sign-up' element={<SignUp/>} />
-	</Routes>
+		<Navbar />
+		<Routes>
+			<Route exact path='/' exact element={<Home />} />
+			<Route path='/about' element={<About/>} />
+			<Route path='/contact' element={<Contact/>} />
+			<Route path='/sign-up' element={<SignUp/>} />
+		</Routes>
 	</Router>
 );
 }
--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -3,7 +3,59 @@ import React from 'react';
 const Home = () => {
 return (
 	<div>
-	<h1>Welcome to GeeksforGeeks</h1>
+		<div class="franchise_banner">
+			<div class="franchise_video_content">
+				<img alt="Hommati" src="./images/Drone Prop Circle Fixed.png" />
+				<h1>Learn everything about owning a Hommati Franchise with the 20 minute Franchise Tour</h1>
+				<button class="franchise_banner_bt">take the franchise tour</button>
+			</div>
+			<div class="franchise_video">
+				<img alt='Hommati' src='./images/Banner1.jpg' />
+			</div>
+
+		</div>
+
+		<section class="franchise_count_area mt-7 mb-3">
+			<div class="container">
+				<div class="row justify-content-md-center">
+					<div class="col-sm col-6">
+						<div class="services_count_box">
+							<img alt="Hommati" src="./images/f_count1.png" />
+							<h4>126</h4>
+							<h6>hommati franchisees</h6>
+						</div>
+					</div>
+					<div class="col-sm col-6">
+						<div class="services_count_box">
+							<img alt="Hommati" src="./images/f_count2.png" />
+							<h4>37</h4>
+							<h6>states hommati is in</h6>
+						</div>
+					</div>
+					<div class="col-sm col-6">
+						<div class="services_count_box">
+							<img alt="Hommati" src="./images/f_count1.png" />
+							<h4>10,918</h4>
+							<h6>agent using hommati</h6>
+						</div>
+					</div>
+					<div class="col-sm col-6">
+						<div class="services_count_box">
+							<img alt="Hommati" src="./images/f_count3.png" />
+							<h4>10,922</h4>
+							<h6>services completed</h6>
+						</div>
+					</div>
+					<div class="col-sm col-6">
+						<div class="services_count_box">
+							<img alt="Hommati" src="./images/f_count4.png" />
+							<h4>2,427,004</h4>
+							<h6>BUYERS SEARCHING ON HOMMATI.COM</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 	</div>
 );
 };
